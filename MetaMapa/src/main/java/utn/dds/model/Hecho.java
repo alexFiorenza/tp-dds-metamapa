7f5c6162a4bf49e32f0d62028aa19373948f7784
IamJuanpax <jubritos@frba.utn.edu.ar>
2025-06-02 16:30:24 -0300

    Commit de la clase hecho (probablemente falten algunas cosas)

--- MetaMapa/src/main/java/utn/dds/model/Hecho.java
+++ MetaMapa/src/main/java/utn/dds/model/Hecho.java
@@ -1,5 +1,33 @@
 package utn.dds.model;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-// Esto es una prueba a aver si funciona
+
 public class Hecho {
+    private String titulo;
+    private String descripcion;
+    private String categoria;
+    private LocalDate fechaAcontecimiento;
+    private Origen origen;    // hay que crear la clase Origen
+    private Contribuyente contribuyente;  // hay que crear la clase Contribuyente
+    private TipoHecho tipo; // hay que crear la clase TipoHecho
+    private Int longitud;
+    private Int latitud;
+    private LocalDate fechaCarga;
+    private EstadoHecho estado;  // hay que crear la clase EstadoHecho
+    private List<String> etiquetas;
+
+    // No se si habria que implementar un constructor
+    public Hecho(){
+
+    }
+
+    public void ocultar(){
+
+    }
+
+    public void activar(){
+
+    }
 }
