0f75ffedb87cce4cca6379bca886f92d458701f7
IamJuanpax <jubritos@frba.utn.edu.ar>
2025-06-02 16:35:22 -0300

    subo la clase contribuytente

--- MetaMapa/src/main/java/utn/dds/model/Contribuyente.java
+++ MetaMapa/src/main/java/utn/dds/model/Contribuyente.java
@@ -1,4 +1,17 @@
 package utn.dds.model;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Contribuyente {
+    private List<Hecho> aportes;
+    private String nombre;
+    private Optional String apellido;    // Creo que asi se ponen opcionalmentre los atributos
+    private String edad;   // No seria mejor usar un INT aca ?
+
+    public Contribuyente() {}   // El constructor por las dudas
+
+    public subirHecho(){
+
+    }
 }
