2ad84ee517dd8895c049e2ffdc5654929d9aea19
Alex Fiorenza <alexfiorenza2012@gmail.com>
2025-06-02 11:06:42 -0300

    CI/CD:Se Agrega initial test & github action

---
+++ MetaMapa/src/test/java/InitialTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class InitialTest {
+    @Test
+    void unoDebeSerUno() {
+        assertEquals(1,1);
+    }
+}
