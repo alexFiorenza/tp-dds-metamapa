67a9833cc5b60c5ee807c67916bc163ed69d8b88
IamJuanpax <jubritos@frba.utn.edu.ar>
2025-06-02 22:27:45 -0300

    Subo tambien la clase SolicitudEliminacion

--- MetaMapa/src/main/java/utn/dds/model/SolicitudEliminacion.java
+++ MetaMapa/src/main/java/utn/dds/model/SolicitudEliminacion.java
@@ -1,4 +1,29 @@
 package utn.dds.model;
 
+import java.time.LocalDateTime;
+
 public class SolicitudEliminacion {
-}
+
+    private String texto;
+    private Hecho hecho;
+    private LocalDateTime fechaSolicitud;
+    private EstadoSolicitud estado;     // Hay que crear esta clase
+    private DetectorSpam detectorSpam;  // Hay que crear esta clase
+
+    // Constructor
+    public SolicitudEliminacion(String texto, Hecho hecho, LocalDateTime fechaSolicitud,
+                                EstadoSolicitud estado, DetectorSpam detectorSpam) {
+        this.texto = texto;
+        this.hecho = hecho;
+        this.fechaSolicitud = fechaSolicitud;
+        this.estado = estado;
+        this.detectorSpam = detectorSpam;
+    }
+
+    public void ocultar() {
+
+    }
+
+    public void activar() {
+
+    }
