1cba55142867d8e4451426db1d87e0821172024e
IamJuanpax <jubritos@frba.utn.edu.ar>
2025-06-08 22:05:02 -0300

    Agregue la primera actualizacion a a la clase contribuyente, aunque todavia le faltan algunas cosas. Esta incluye crear un hecho, pero falta enviar la creacion para que sea o no aprobada.

--- MetaMapa/src/main/java/utn/dds/model/Contribuyente.java
+++ MetaMapa/src/main/java/utn/dds/model/Contribuyente.java
@@ -1,4 +1,5 @@
 package utn.dds.model;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +18,23 @@ public class Contribuyente {
         this.edad = edad;
         this.aportes = new ArrayList<>();
     }
+
+    // No se si se deberian pasar las fuentes por atributo, y tampoco si esto seria repeticion de codigo por lo de Hecho
+    public void crearHecho(String titulo, String descripcion, String categoria, LocalDate fechaAcontecimiento,
+                           Origen origen, TipoHecho tipo,
+                           double longitud, double latitud, LocalDate fechaCarga,
+                           EstadoHecho estado, List<String> etiquetas, String uuid){
+        // No sabria si tengo que poner asi el atributo uuid
+
+        Contribuyente info = new Contribuyente(this.nombre, this.apellido, this.edad);
+
+        Hecho nuevoHecho = new Hecho(titulo, descripcion, categoria, fechaAcontecimiento,
+                                    origen, info, tipo, longitud, latitud, fechaCarga,
+                                    estado, etiquetas, uuid);
+
+        // aca no se si añadirlo directamente a la lista de aportes, o si tengo que hacer otrafuncion para
+        // saber si fue aceptada la subida del hecho o no
+        aportes.add(nuevoHecho);
+    }
+
 }
