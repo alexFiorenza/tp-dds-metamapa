d2429c7a5e4b0e38d7106d635613937b1aa87fe4
IamJuanpax <jubritos@frba.utn.edu.ar>
2025-06-09 00:10:58 -0300

    Creo con Party las funciones del ContribuyenteService, pero falta la de  obtenerHechos. Ademas saque las funciones de la clase Contribuyente.

--- MetaMapa/src/main/java/utn/dds/model/Contribuyente.java
+++ MetaMapa/src/main/java/utn/dds/model/Contribuyente.java
@@ -1,4 +1,6 @@
 package utn.dds.model;
+import utn.dds.service.negocio.ContribuyenteService;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,22 +21,34 @@ public class Contribuyente {
         this.aportes = new ArrayList<>();
     }
 
-    // No se si se deberian pasar las fuentes por atributo, y tampoco si esto seria repeticion de codigo por lo de Hecho
+    // Invoco al servicio
+    ContribuyenteService servicio = new ContribuyenteService();
+
     public void crearHecho(String titulo, String descripcion, String categoria, LocalDate fechaAcontecimiento,
                            Origen origen, TipoHecho tipo,
                            double longitud, double latitud, LocalDate fechaCarga,
-                           EstadoHecho estado, List<String> etiquetas, String uuid){
-        // No sabria si tengo que poner asi el atributo uuid
+                           EstadoHecho estado, List<String> etiquetas){
 
-        Contribuyente info = new Contribuyente(this.nombre, this.apellido, this.edad);
-
-        Hecho nuevoHecho = new Hecho(titulo, descripcion, categoria, fechaAcontecimiento,
-                                    origen, info, tipo, longitud, latitud, fechaCarga,
-                                    estado, etiquetas, uuid);
-
-        // aca no se si añadirlo directamente a la lista de aportes, o si tengo que hacer otrafuncion para
-        // saber si fue aceptada la subida del hecho o no
-        aportes.add(nuevoHecho);
+        servicio.crearHecho(this, titulo, descripcion, categoria, fechaAcontecimiento, origen, tipo,
+                                    longitud, latitud, fechaCarga, estado, etiquetas);
     }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public List<Hecho> getAportes() {
+        return aportes;
+    }
+
 
 }
--- MetaMapa/src/main/java/utn/dds/service/negocio/ContribuyenteService.java
+++ MetaMapa/src/main/java/utn/dds/service/negocio/ContribuyenteService.java
@@ -1,4 +1,29 @@
 package utn.dds.service.negocio;
 
+import utn.dds.model.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class ContribuyenteService {
+    // No se si se deberian pasar las fuentes por atributo, y tampoco si esto seria repeticion de codigo por lo de Hecho
+    public void crearHecho(Contribuyente info, String titulo, String descripcion, String categoria, LocalDate fechaAcontecimiento,
+                           Origen origen, TipoHecho tipo,
+                           double longitud, double latitud, LocalDate fechaCarga,
+                           EstadoHecho estado, List<String> etiquetas){
+
+        Hecho nuevoHecho = new Hecho(titulo, descripcion, categoria, fechaAcontecimiento,
+                origen, info, tipo, longitud, latitud, fechaCarga,
+                estado, etiquetas);
+
+        // aca no se si añadirlo directamente a la lista de aportes, o si tengo que hacer otrafuncion para
+        // saber si fue aceptada la subida del hecho o no
+        //aportes.add(nuevoHecho);
+    }
+
+    /*
+    public List<Hecho>obtenerHechos(int Hora){
+        // usar un filter para que me devuelva todos los hechos mayores o menores a esa hora
+    }
+     */
 }
