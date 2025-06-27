5e615a0cdb4c7850d11980168875a2039952d88f
Alex Fiorenza <alexfiorenza2012@gmail.com>
2025-06-26 21:17:09 -0300

    Feat: Agrego DTO a controller estatica y redis

--- apps/fuentes/estatica/src/main/java/utn/dds/fuentes/estatica/ControllerFuenteEstatica.java
+++ apps/fuentes/estatica/src/main/java/utn/dds/fuentes/estatica/ControllerFuenteEstatica.java
@@ -2,8 +2,10 @@ package utn.dds.fuentes.estatica;
 
 import io.javalin.http.Context;
 import utn.dds.dominio.Hecho;
+import utn.dds.dto.HechoDTO;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ControllerFuenteEstatica {
     private final ServiceFuenteEstatica estaticaService;
@@ -15,7 +17,10 @@ public class ControllerFuenteEstatica {
     public void obtenerHechos(Context ctx) {
         try {
             List<Hecho> hechos = estaticaService.obtenerHechos();
-            ctx.json(hechos);
+            List<HechoDTO> hechosDTO = hechos.stream()
+                .map(HechoDTO::fromHecho)
+                .collect(Collectors.toList());
+            ctx.json(hechosDTO);
         } catch (Exception e) {
             ctx.status(500).result("Error al obtener hechos: " + e.getMessage());
         }
