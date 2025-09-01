27d17d16d62d390fb1cc4c9b8247d0ba21e9de57
IamJuanpax <jubritos@frba.utn.edu.ar>
2025-08-31 21:47:14 -0300

    comentarios e ideas

--- apps/fuentes/dinamica/src/main/java/utn/dds/fuentes/dinamica/services/ServiceHechoDinamica.java
+++ apps/fuentes/dinamica/src/main/java/utn/dds/fuentes/dinamica/services/ServiceHechoDinamica.java
@@ -4,23 +4,34 @@ import utn.dds.daos.IDAO;
 import utn.dds.dominio.Hecho;
 import utn.dds.dto.HechoDTO;
 import utn.dds.fuentes.dinamica.FuenteDinamicaImpl;
+import utn.dds.fuentes.dinamica.conexion.Conexion;
 import utn.dds.fuentes.dinamica.repositories.HechoRepository;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 public class ServiceHechoDinamica {
     private final HechoRepository repository;
     private final FuenteDinamicaImpl fuenteDeDatos;
+    private final URL url;
     
-    public ServiceHechoDinamica(IDAO<Hecho> dao, FuenteDinamicaImpl fuenteDeDatos) {
-        this.repository = new HechoRepository(dao);
-        this.fuenteDeDatos = fuenteDeDatos;
+    public ServiceHechoDinamica(String daoType, Map<String, Object> daoConfig, Conexion conexion) {
+        try {
+            // Mepa que esto deberia ser el  mock de los hechos que tengo en la carpeta resources
+            this.url= URI.create("http://example.com/api/hechos").toURL();
+        } catch (MalformedURLException | IllegalArgumentException e) {
+            throw new RuntimeException("Error al crear URL", e);
+        }
+
+        this.repository = new HechoRepository(daoType, daoConfig);
+        this.fuenteDeDatos = new FuenteDinamicaImpl(conexion, this.url);
     }
-    
+
+    // Aca ver si obtengo los hechos desde el repositorio o desde la fuente
     public List<Hecho> obtenerHechos() throws IOException {
         return repository.obtenerHechos();
     }
