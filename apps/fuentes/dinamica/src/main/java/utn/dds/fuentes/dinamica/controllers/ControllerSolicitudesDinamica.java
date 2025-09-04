252ebd9b915c4fe86567b8169e897ee82fc902c3
tomasfiestas <tfiestasescubilla@frba.utn.edu.ar>
2025-09-04 16:14:40 -0300

    fix: Agregamos con juanpa loggers para encontrar el error de solicitudes, en proceso.

--- apps/fuentes/dinamica/src/main/java/utn/dds/fuentes/dinamica/controllers/ControllerSolicitudesDinamica.java
+++ apps/fuentes/dinamica/src/main/java/utn/dds/fuentes/dinamica/controllers/ControllerSolicitudesDinamica.java
@@ -5,8 +5,11 @@ import utn.dds.dominio.Hecho;
 import utn.dds.dominio.SolicitudEliminacion;
 import utn.dds.dto.HechoDTO;
 import utn.dds.dto.SolicitudEliminacionDTO;
+import utn.dds.fuentes.dinamica.Main;
 import utn.dds.fuentes.dinamica.services.ServiceHechoDinamica;
 import utn.dds.fuentes.dinamica.services.ServiceSolicitudesDinamica;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.stream.Collectors;
 
 public class ControllerSolicitudesDinamica {
     private final ServiceSolicitudesDinamica solicitudesService;
+    private static final Logger loggerSolicitudes = LoggerFactory.getLogger(ControllerSolicitudesDinamica.class);
 
     public ControllerSolicitudesDinamica(String daoType, Map<String, Object> daoConfig){
         this.solicitudesService = new ServiceSolicitudesDinamica(daoType, daoConfig);
@@ -22,11 +26,16 @@ public class ControllerSolicitudesDinamica {
 
     public void obtenerSolicitudesDeEliminacion(Context ctx){
         try {
+            loggerSolicitudes.info("-----------Obteniendo Solicitudes de Eliminación-----------");
             List<SolicitudEliminacion> solicitudesEliminacion = this.solicitudesService.obtenerSolicitudes();
+
+            loggerSolicitudes.info("Tamaño solicitud eliminacion es: " + solicitudesEliminacion.size());
+
             List<SolicitudEliminacionDTO> solicitudesDTO = solicitudesEliminacion.stream()
                     .map(SolicitudEliminacionDTO::fromSolicitudEliminacion)
                     .collect(Collectors.toList());
             ctx.json(solicitudesDTO);
+            loggerSolicitudes.info("Se obtuvieron exitosamente las solicitudes de eliminación");
         } catch (Exception e) {
             ctx.status(500).result("Error al obtener solicitudes de eliminacion: " + e.getMessage());
         }
@@ -34,10 +43,12 @@ public class ControllerSolicitudesDinamica {
 
     public void agregarSolicitudDeEliminacionDeHecho(Context ctx) {
         try {
+            loggerSolicitudes.info("-----------Agregando Solicitud de Eliminación-----------");
             SolicitudEliminacionDTO solicitudDTO = ctx.bodyAsClass(SolicitudEliminacionDTO.class);
             SolicitudEliminacion solicitud = solicitudDTO.toSolicitudEliminacion();
             this.solicitudesService.agregarSolicitud(solicitud);
             ctx.json(solicitudDTO);
+            loggerSolicitudes.info("Se agrego exitosamente la solicitud de eliminación");
         } catch (Exception e) {
             ctx.status(500).result("Error al agregar solicitud de eliminacion: " + e.getMessage());
         }
