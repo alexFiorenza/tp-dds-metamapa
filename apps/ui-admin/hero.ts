b169ff7aeac63d38f8a4c59fc65ddff1039d0313
Alex Fiorenza <alexfiorenza2012@gmail.com>
2025-10-22 11:09:27 -0300

    Fix: Arreglos en hero.ts colores

--- apps/ui-admin/hero.ts
+++ apps/ui-admin/hero.ts
@@ -5,29 +5,29 @@ export default heroui({
     light: {
       colors: {
         primary: {
-          DEFAULT: "oklch(0.5417 0.1790 288.0332)",
-          foreground: "oklch(1.0000 0 0)",
+          DEFAULT: "#8B5CF6",
+          foreground: "#FFFFFF",
         },
         secondary: {
-          DEFAULT: "oklch(0.9174 0.0435 292.6901)",
-          foreground: "oklch(0.4143 0.1039 288.1742)",
+          DEFAULT: "#F3E8FF",
+          foreground: "#6B21A8",
         },
-        background: "oklch(0.9730 0.0133 286.1503)",
-        foreground: "oklch(0.3015 0.0572 282.4176)",
+        background: "#FEFBFF",
+        foreground: "#4C1D95",
       },
     },
     dark: {
       colors: {
         primary: {
-          DEFAULT: "oklch(0.7162 0.1597 290.3962)",
-          foreground: "oklch(0.1743 0.0227 283.7998)",
+          DEFAULT: "#A78BFA",
+          foreground: "#1E1B4B",
         },
         secondary: {
-          DEFAULT: "oklch(0.3139 0.0736 283.4591)",
-          foreground: "oklch(0.8367 0.0849 285.9111)",
+          DEFAULT: "#5B21B6",
+          foreground: "#DDD6FE",
         },
-        background: "oklch(0.1743 0.0227 283.7998)",
-        foreground: "oklch(0.9185 0.0257 285.8834)",
+        background: "#1E1B4B",
+        foreground: "#F3E8FF",
       },
     },
   },
